8ca90b124ab602b3894919badf4be7efc855d086
brandonsterling <brandonsterling@knights.ucf.edu>
2022-09-02 18:58:18 -0400

    replacing cors policy method

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const cors = require("cors");
+const cors = require("cors");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const dbConnect = require("./db/dbConnect");
@@ -8,14 +8,14 @@ const User = require("./models/user");
 const Resume = require("./models/resume");
 
 const app = express();
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: '',
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // body parser configuration
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,18 +23,18 @@ app.use(bodyParser.json());
 
 dbConnect();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+//   );
+//   next();
+// });
 
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
